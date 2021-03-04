55b6c71d72d05268d892352a042894f1bf9bc198
Melanie <mel.lontsi@gmail.com>
2021-03-04 12:54:15 -0800

    changes storage system to AWS S3

--- app/app/settings.py
+++ app/app/settings.py
@@ -164,7 +164,7 @@ if USE_S3:
     # s3 static settings
     STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'app.storage_backends.StaticStorage'
+    STATICFILES_STORAGE = 'app.storage_backends.StaticMediaStorage'
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'translations'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
